257baf572c8c3f65ad5811b62fc5f0643d5074f6
mp107 <mp107@o2.pl>
2025-12-31 23:04:20 +0100

    ci.yml: Use libapache2-mod-fcgid in tests/ci/install.sh

--- tests/ci/install.sh
+++ tests/ci/install.sh
@@ -12,7 +12,7 @@ echo "opcache.enable = 0" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.i
 
 ~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
 
-sudo a2enmod rewrite actions fastcgi alias ssl
+sudo a2enmod rewrite actions fcgid alias ssl
 
 sudo cp -f tests/ci/apache_vhost.conf /etc/apache2/sites-available/default
 sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/default
